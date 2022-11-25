c849546fa2a9621eb1121d95dde1555379c0c766
sahak <sahakyan18@gmail.com>
2022-11-25 20:28:58 +0400

    KKKKK

--- LLLL.py
+++ LLLL.py
@@ -12,3 +12,7 @@ for country, city in my_dictionary.items():
     print(country, city)
     for s in city:
         print(s)
+        for country, city in my_dictionary.items():
+            print(country, city)
+            for s in city:
+                print(s)
