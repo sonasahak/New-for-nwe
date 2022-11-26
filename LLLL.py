e7b4a932ec6150a578f1b17f4795fa1276849b8f
sahak <sahakyan18@gmail.com>
2022-11-26 18:10:43 +0400

    gggggf

--- LLLL.py
+++ LLLL.py
@@ -24,7 +24,37 @@ for country, city in my_dictionary.items():
 
 
 
+
+
+
+
+
+
   for country, city in my_dictionary.items():
             print(country, city)
             for s in city:
                 print(s)
+
+                # city= [["Yerevan", "Abovyan"], ["Dilijan", "Armavir"], ["Talin", "Ijevan"]]
+                # for c in city:
+                #   print(c)
+
+                cities = [["Yerevan", "Abovyan"], ["Dilijan", "Armavir"], ["Talin", "Ijevan"]]
+                # for country, city in cities:
+                #  print("country is "+ country + " and city  is " +city )
+                my_dictionary = dict(cities)
+                print(my_dictionary)
+
+                for country, city in my_dictionary.items():
+                    print(country, city)
+                    for s in city:
+                        print(s)
+                        for country, city in my_dictionary.items():
+                            print(country, city)
+                            for s in city:
+                                print(s)
+
+                for country, city in my_dictionary.items():
+                    print(country, city)
+                    for s in city:
+                        print(s)
